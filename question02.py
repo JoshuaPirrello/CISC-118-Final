e1a44b5f4549bd9f61ad05742c7e8d34d4ad66d7
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-05-13 08:21:54 -0400

    completed question02.py

---
+++ question02.py
@@ -0,0 +1,8 @@
+print("          __")
+print("         /'{>")
+print("     ____) (____")
+print("    //'--; ;--'\\\\")
+print("  ///////\\_/\\\\\\\\\\\\\\")
+print("         m m")
+
+# this will properly recreate the ASCII art of the eagle in python using only the print function and strings 
