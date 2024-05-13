c39c6487ca97603b9fdfc873be210e2ab47d29ed
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-05-13 08:12:37 -0400

    Update question01.py

---
+++ question01.py
@@ -0,0 +1,9 @@
+first_name = input("Please enter your first name: ")
+
+
+last_name = input("Please enter your last name: ")
+
+
+print("Welcome:", first_name, last_name)
+
+# This allows the program to ask the user to type in their first name, than their last and prints both 
