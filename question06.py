f5933d985af0ce69a5d26b196c10afb5e7115fb1
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-05-13 08:33:10 -0400

    Update question06.py

---
+++ question06.py
@@ -0,0 +1,5 @@
+#this code will find the difference between the values x and t, in this case 133 and 245
+def find_difference(x, y):
+    return x - y
+Answer = find_difference(133, 245)
+print(Answer)
