4986a8ed13820db709d1113060ebce31ad838c37
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-05-13 08:26:04 -0400

    Update question03.py

---
+++ question03.py
@@ -0,0 +1,8 @@
+#Create a “while” loop that will print the even numbers from 100 down to 2,
+# make it inclusive
+num = 100
+while num >= 2:
+    if num % 2 == 0:
+        print(num)
+    num -= 2
+#This code counts the even numbers, up to 100, and does not skip over 2
